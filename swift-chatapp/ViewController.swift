ad562d8349015666332ac7c49c5479e24d3be0ef
高畑 孝輝 <takahata@takabata-no-MacBook-Air.local>
2016-05-14 17:47:57 +0900

    create project

---
+++ swift-chatapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-chatapp
+//
+//  Created by 高畑 孝輝 on 2016/05/14.
+//  Copyright © 2016年 japan-alps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
